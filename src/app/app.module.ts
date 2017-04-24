988431370efd6fd9701620f3710b87944f31e21f
Suhail, Mohammed | DU <mohammed.suhail@rakuten.com>
2017-04-24 11:21:20 +0530

    commit!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ProductListComponent } from './products/product-list.component'
 
 import { AppComponent } from './app.component';
+import {ProductFilterPipe} from './products/product-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      ProductListComponent,
+       ProductFilterPipe
   ],
   imports: [
     BrowserModule,
